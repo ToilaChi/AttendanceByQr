9ad8f609149a65b5c4deaa13febbf7588382acaa
ToilaChi <chi12345pham@gmail.com>
2025-05-14 23:08:19 +0700

    add service eureka

--- Microservices/auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ Microservices/auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -3,9 +3,11 @@ package com.example.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AuthServiceApplication {
 
   public static void main(String[] args) {
